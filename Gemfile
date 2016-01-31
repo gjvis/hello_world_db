505d8adf05560cd13748a6260cc2d5c0257a94e7
Gareth Visagie <gareth@gjvis.com>
2016-01-31 20:07:12 +0000

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "https://rubygems.org"
+
+gem "pg"
+gem "sequel"
+gem "sinatra"
+gem "unicorn"
---
+++ app.rb
@@ -0,0 +1,8 @@
+require "sinatra"
+require "sequel"
+
+DB = Sequel.connect(ENV["HELLO_WORLD_DB"])
+
+get "/" do
+  "Hello #{DB[:names].map(:name).join(", ")}!"
+end
